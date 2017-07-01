b4e942a9ae563d622040ef7d3cfbfd4ae45323c5
Erik M. Bray <erik.m.bray+iguananaut@gmail.com>
2017-07-01 21:49:28 +0200

    The mess you have to go through with webpack just to get it to copy an HTML file

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: "./src/generate.js",
-  output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "bundle.js",
-      library: "YSNG"
-  }
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        library: "YSNG"
+    },
+    module: {
+        loaders: [
+            /* Just copy all HTML files (of which there is only one) */
+            {
+                test: /\.html$/,
+                loader: "file-loader",
+                query: "name=[name].[ext]"
+            }
+        ]
+    }
 }
