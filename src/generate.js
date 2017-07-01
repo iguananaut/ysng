fe54bf96ed4a4a7d82bba76ca32737c4474a2dfc
Erik M. Bray <erik.m.bray+iguananaut@gmail.com>
2017-07-01 14:42:17 +0200

    Random chance for letters replaced with numbers

--- src/generate.js
+++ src/generate.js
@@ -1,9 +1,52 @@
 const GRAMMAR = require("./grammar");
 const PROD_RE = new RegExp("<([^>]+)>", "g");
+const LETTER_SUBSTS = {
+    "o": "0",
+    "e": "3",
+    "i": "1",
+    "a": "4"
+}
 
 
 var randomChoice = function(choices) {
     return choices[Math.floor(Math.random() * choices.length)];
+}
+
+
+/* This would be easier with modern ES2016 but I'll set that up later;
+ * this will work on most modern browsers */
+var substituteLetters = function(name) {
+    var letters = [];
+    var nameLower = name.toLowerCase();
+    Object.keys(LETTER_SUBSTS).forEach(function(letter) {
+        if (nameLower.indexOf(letter) >= 0) {
+            letters.push(letter);
+        }
+    });
+
+    if (letters.length == 0) {
+        return name;
+    }
+
+    /* If there are any replaceable letters, replace them with, say, 20%
+     * chance */
+    if (Math.random() < 0.2) {
+        /* Choose a random letter of the ones we can replace */
+        var letter = randomChoice(letters);
+        var indices = [], idx = -1;
+        while ((idx = nameLower.indexOf(letter, idx + 1)) >= 0) {
+            indices.push(idx);
+        }
+
+        /* Now choose a random index at which to replace the chosen letter */
+        idx = randomChoice(indices);
+        /* Call substituteLetters recursively for a chance to replace more
+         * letters*/
+        return substituteLetters(name.substr(0, idx) + LETTER_SUBSTS[letter] +
+                                 name.substr(idx + 1));
+    }
+
+    return name;
 }
 
 
@@ -40,7 +83,7 @@ var generateFromGrammar = function(grammar, symbol) {
 
 
 var generateName = function() {
-    return generateFromGrammar(GRAMMAR, "name");
+    return substituteLetters(generateFromGrammar(GRAMMAR, "name"));
 }
 
 
