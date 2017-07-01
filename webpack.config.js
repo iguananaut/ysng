fe8109276c94a239ff2b1f9e373d7c539a8f63d7
Erik M. Bray <erik.m.bray+iguananaut@gmail.com>
2017-07-01 15:50:43 +0200

    Output webpack output to a dist/ directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
+const path = require('path');
+
 module.exports = {
   entry: "./src/generate.js",
   output: {
+      path: path.resolve(__dirname, "dist"),
       filename: "bundle.js",
       library: "YSNG"
   }
